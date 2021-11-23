2eeb7ab2f694865f006bf550721efdc106e73b91
Emmanuel Ochoa <eorivera95@gmail.com>
2021-11-22 21:25:50 -0700

    Creating the Calculator class to pass the test

--- src/calc.js
+++ src/calc.js
@@ -2,4 +2,14 @@ function add(x, y) {
   return x + y;
 }
 
-module.exports = add;
+class Calculator {
+  constructor(options) {
+    this.precision = options.precision;
+  }
+  add(x, y) {
+    const sum = x + y;
+    return Number(sum.toFixed(this.precision));
+  }
+}
+
+module.exports = { add, Calculator };
