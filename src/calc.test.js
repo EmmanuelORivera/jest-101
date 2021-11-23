0a45fea1b4d15173031790912f3de95a8e21341f
Emmanuel Ochoa <eorivera95@gmail.com>
2021-11-22 20:52:20 -0700

    grouping relevant tests together

--- src/calc.test.js
+++ src/calc.test.js
@@ -3,7 +3,15 @@ const add = require("./calc");
 //Test suit
 describe("calculator", () => {
   //Test cases
-  it("add two numbers", () => {
-    expect(add(1, 2)).toEqual(3);
+  describe("should perform addition", () => {
+    it("adds two positive numbers", () => {
+      expect(add(1, 2)).toEqual(3);
+    });
+    it("adds two negative numbers", () => {
+      expect(add(-10, -24)).toEqual(-34);
+    });
+    it("adds one positive number and one negative number", () => {
+      expect(add(5, -3)).toEqual(2);
+    });
   });
 });
