74e85d4d4621f35b01ac92fe4ea4d3e1b803c40c
Emmanuel Ochoa <eorivera95@gmail.com>
2021-11-23 20:20:35 -0700

    extending the expect function to create a brand new matcher

--- src/expectFunction.spec.js
+++ src/expectFunction.spec.js
@@ -1,6 +1,25 @@
 //creating my own matchers
 
-const { expect, it } = require("@jest/globals");
+import { expect, it } from "@jest/globals";
+import jsonpath from "jsonpath";
+
+expect.extend({
+  toMatchJsonPath(received, argument) {
+    const result = jsonpath.query(received, argument);
+    if (result.length > 0) {
+      return {
+        pass: true,
+        message: () => "matched",
+      };
+    } else {
+      return {
+        pass: false,
+        message: () =>
+          `expected ${JSON.stringify(received)} to match jsonpath ${argument}`,
+      };
+    }
+  },
+});
 
 it("string contains", () => {
   const givenName = expect.stringContaining("Emmanuel");
@@ -23,12 +42,34 @@ describe("api", () => {
     projects: [{ name: "first project" }, { name: "second project" }],
   };
 
-  const matcher = expect.objectContaining({
-    name: expect.stringContaining("Emmanuel"),
-    projects: expect.arrayContaining([
-      { name: expect.stringContaining("project") },
-    ]),
+  it("checks contains Emmanuel as a name and project as a name from projects", () => {
+    const matcher = expect.objectContaining({
+      name: expect.stringContaining("Emmanuel"),
+      projects: expect.arrayContaining([
+        { name: expect.stringContaining("project") },
+      ]),
+    });
+
+    expect(user).toEqual(matcher);
   });
 
-  expect(user).toEqual(matcher);
+  const result = jsonpath.query(user, "$.projects");
+  console.log(JSON.stringify(result));
+});
+
+describe("jsonpath", () => {
+  it("matches jsonpath", () => {
+    const user = {
+      name: "Juan",
+    };
+    expect(user).toMatchJsonPath("$.name");
+  });
+  it("does not match jsonpath", () => {
+    const user = {
+      name: "Emmanuel",
+      address: "Chihuahua",
+    };
+
+    expect(user).not.toMatchJsonPath("$.age");
+  });
 });
