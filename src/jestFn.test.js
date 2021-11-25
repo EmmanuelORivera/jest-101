c34c5b737421e26495e5c4cb9de31a7828400782
Emmanuel Ochoa <eorivera95@gmail.com>
2021-11-24 23:11:18 -0700

    mock implementation of the global fetch

--- src/jestFn.test.js
+++ src/jestFn.test.js
@@ -1,3 +1,7 @@
+const fetchUser = (id, process) => {
+  return fetch(`http://localhost:4000/users/${id}`);
+};
+
 describe("jest.fn to set up a spy function", () => {
   const mock = jest.fn();
   mock("Emmanuel");
@@ -17,5 +21,25 @@ describe("jest.fn to set up a spy function", () => {
 
     expect(fakeAdd(1, 1)).toBe(5);
     expect(fakeAdd).toHaveBeenCalledWith(1, 1);
+  });
+});
+
+describe("mock API call", () => {
+  const user = {
+    name: "Emmanuel",
+  };
+
+  it("mock fetch", () => {
+    global.fetch = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve({ user }));
+
+    const process = jest.fn();
+
+    fetchUser(111).then((x) => console.log(x));
+
+    expect(global.fetch).toHaveBeenCalledWith(
+      "http://localhost:4000/users/111"
+    );
   });
 });
